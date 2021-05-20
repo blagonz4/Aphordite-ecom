88f5b4c2f99c0afeb2cd7b8f425b0192e3b84fa7
Dat <DatNguyen777>
2021-05-20 17:50:26 +0700

    sua field host trong file databases

--- databases/databases.js
+++ databases/databases.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     'user', //pasword
     {
         dialect:'postgres',
-        host:'localhost',
+        host:process.env.PORT,
         operatorsAliases: false,
         pool:{
             max:5,
