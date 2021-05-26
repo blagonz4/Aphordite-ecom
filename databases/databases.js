f44cb4e0687ab459b5c84314a3713b93eaf49928
Dat <DatNguyen777>
2021-05-26 20:29:18 +0700

    test create table

--- databases/databases.js
+++ databases/databases.js
@@ -17,7 +17,13 @@ const sequelize = new Sequelize(
         }
     
 );
-
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL ||
+    `postgres://nswdegztbkglav:cdba805c32d6b9eeb06d32b4d589751fbbc7338a4a78d73dc590fa465e97c795@ec2-34-193-112-164.compute-1.amazonaws.com:5432/da99l1inkem70g`,
+    {
+        logging: false,
+        ssl: { rejectUnauthorized: false } //solved the problem with self signed sertificate
+    } )
 const Op = Sequelize.Op;
 module.exports = {
     sequelize,
