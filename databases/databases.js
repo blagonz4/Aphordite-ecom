49791562bcd46bf00c0064acb5e75108f3137edd
Dat <DatNguyen777>
2021-05-26 16:49:12 +0700

    test data url

--- databases/databases.js
+++ databases/databases.js
@@ -1,22 +1,22 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-    'da99l1inkem70g',//database name
-    'nswdegztbkglav', //user name
-    'cdba805c32d6b9eeb06d32b4d589751fbbc7338a4a78d73dc590fa465e97c795', //pasword
-    {
-        dialect:'postgres',
-        host:'localhost',
-        port:'5432',
-        operatorsAliases: false,
-        pool:{
-            max:5,
-            min:0,
-            require:5000,
-            idle:10000
-        }
-    }
-);
-
+// const sequelize = new Sequelize(
+//     'postgres',//database name
+//     'postgres', //user name
+//     'user', //pasword
+//     {
+//         dialect:'postgres',
+//         host:'localhost',
+//         port:'5432',
+//         operatorsAliases: false,
+//         pool:{
+//             max:5,
+//             min:0,
+//             require:5000,
+//             idle:10000
+//         }
+//     }
+// );
+const sequelize = new Sequelize('postgres://postgres:user@:5432/postgres')
 const Op = Sequelize.Op;
 module.exports = {
     sequelize,
