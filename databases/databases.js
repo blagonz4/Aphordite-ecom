20bfa831db55220a3f5c4a42a1043c42e5134ce6
Dat <DatNguyen777>
2021-05-26 20:34:14 +0700

    test create table

--- databases/databases.js
+++ databases/databases.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-        'da99l1inkem70g',//database name
-        'nswdegztbkglav', //user name
-        'cdba805c32d6b9eeb06d32b4d589751fbbc7338a4a78d73dc590fa465e97c795', //pasword
+        'postgres',//database name
+        'postgres', //user name
+        'user', //pasword
         {
             dialect:'postgres',
-            host:'ec2-34-193-112-164.compute-1.amazonaws.com',
+            host:'localhost',
             port:'5432',
             operatorsAliases: false,
             pool:{
@@ -17,12 +17,12 @@ const sequelize = new Sequelize(
         }
     
 );
-const sequelize = new Sequelize(
-    'postgres://nswdegztbkglav:cdba805c32d6b9eeb06d32b4d589751fbbc7338a4a78d73dc590fa465e97c795@ec2-34-193-112-164.compute-1.amazonaws.com:5432/da99l1inkem70g',
-    {
-        logging: false,
-        ssl: { rejectUnauthorized: false } //solved the problem with self signed sertificate
-    } )
+// const sequelize = new Sequelize(
+//     'postgres://nswdegztbkglav:cdba805c32d6b9eeb06d32b4d589751fbbc7338a4a78d73dc590fa465e97c795@ec2-34-193-112-164.compute-1.amazonaws.com:5432/da99l1inkem70g',
+//     {
+//         logging: false,
+//         ssl: { rejectUnauthorized: false } //solved the problem with self signed sertificate
+//     } )
 const Op = Sequelize.Op;
 module.exports = {
     sequelize,
