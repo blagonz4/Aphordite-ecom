34e28c0ea5b66f2278407b6c20dc57a83b1ff0c5
Dat <DatNguyen777>
2021-05-26 14:11:47 +0700

    dm

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Aphrodite' });
-  console.log("Sever is running at http://localhost:%d",port);
 });
 
 module.exports = router;
